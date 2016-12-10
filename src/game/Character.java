6bbb6bd9fc127eeeef09b40f385336c21ea72098
JayCee235 <caudejn@rose-hulman.edu>
2016-12-09 23:11:49 -0500

    weird character stuff done

--- src/game/Character.java
+++ src/game/Character.java
@@ -1,14 +1,86 @@
 package game;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import gui.Drawable;
 
-public class Character implements Drawable{
-	int x, y;
+public class Character implements Drawable, KeyListener, MouseListener{
+	int x, y, w, h;
+	Image sprite;
+	
+	public Character(String spritePath) {
+		this.x = 0;
+		this.y = 0;
+		try {
+			sprite = ImageIO.read(new File(spritePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.w = 8;
+		this.h = 8;
+	}
 	
 	@Override
 	public void draw(Graphics g) {
+		int dx = x * Drawable.SCALE * w;
+		int dy = y * Drawable.SCALE * h;
+		g.translate(dx, dy);
+		g.drawImage(sprite, 0, 0, w*Drawable.SCALE, h*Drawable.SCALE, 0, 0, w, h, null);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/game/GameMap.java
+++ src/game/GameMap.java
@@ -10,10 +10,15 @@ public class GameMap implements Drawable {
 	Tile[][] tiles;
 	Map<String, Tile> tileTypes;
 	
+	Character c;
+	
 	public GameMap(int x, int y) {
 		tiles = new Tile[x][y];
 		tileTypes = new HashMap<String, Tile>();
 		tileTypes.put("testGrass", new TestGrassTile("./res/testGrass.png"));
+		c = new Character("./res/testChar.png");
+		c.x = 10;
+		c.y = 10;
 		
 		for(int i = 0; i < x; i++) {
 			for(int j = 0; j < y; j++) {
@@ -34,6 +39,7 @@ public class GameMap implements Drawable {
 				g.translate(-dx, -dy);
 			}
 		}
+		c.draw(g);
 	}
 	
 	public Tile getTile(int x, int y) {
